fd3436025e3603db6d09fd49c36c4ba2e2edaa9d
Karl Zylinski <karl@zylinski.se>
2019-02-04 18:04:58 +0100

    can find stars near each other...

--- find_stars.py
+++ find_stars.py
@@ -1,4 +1,5 @@
 import sqlite3
+import math
 
 dry_run = False
 dry_run_print = True
@@ -16,14 +17,32 @@ all_stars = c.execute("SELECT source_id, ra, dec, parallax, pmra, pmdec, radial_
 # 6 radial_velocity
 # 7 distance
 
+max_sep = 1 # maximal separation, pc
+deg_to_rad = math.pi/180
+
 for s in all_stars:
+    sid = s[0]
     ra = s[1] # mas
     dec = s[2] # mas
     d = s[7] # distance in pc
+    min_d = d - max_sep
+    max_d = d + max_sep
+    ra_rad = ra * deg_to_rad
+    dec_rad = dec * deg_to_rad
+    min_radir_sep = (ra_rad - max_sep/d)/deg_to_rad;
+    max_radir_sep = (ra_rad + max_sep/d)/deg_to_rad;
+    min_decdir_sep = (dec_rad - max_sep/d)/deg_to_rad;
+    max_decdir_sep = (dec_rad + max_sep/d)/deg_to_rad;
 
     comoving_stars = c.execute('''
-        SELECT source_id, ra, dec, parallax, pmra, pmdec, radial_velocity
+        SELECT source_id, phot_g_mean_mag
         FROM gaia
-        WHERE ''').fetchall()
+        WHERE source_id IS NOT %d
+        AND distance > %d AND distance < %d
+        AND ra > %d AND ra < %d
+        AND dec > %d AND dec < %d''' % (sid, min_d, max_d, min_radir_sep, max_radir_sep, min_decdir_sep, max_decdir_sep)).fetchall()
+
+    if len(comoving_stars) > 0:
+        print(comoving_stars)
 
 conn.close()
